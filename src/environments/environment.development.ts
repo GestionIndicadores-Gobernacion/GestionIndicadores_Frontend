aff07487d6b5df764630b286b9c07b65157cb83b
Milo <jcrodriguezc3@correo.usbcali.edu.co>
2025-12-16 15:35:11 -0500

    .env desplegando

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://gestionindicadores-backend.onrender.com',
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://gestionindicadores-backend.onrender.com',
+  // apiUrl: 'http://localhost:5000',
 };
