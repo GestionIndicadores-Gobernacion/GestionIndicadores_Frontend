e5281dcc04c43ec9a0eb0dff7b4f72841cbd22c8
Milo <jcrodriguezc3@correo.usbcali.edu.co>
2026-01-19 10:46:55 -0500

    localstorage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth-guard';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout';
-import { adminGuard } from './core/guards/admin-guard';
 
 
 export const routes: Routes = [
--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,46 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { catchError, map, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = () => {
-
   const router = inject(Router);
   const auth = inject(AuthService);
 
-  const token = auth.getAccessToken();
-
-  // ❌ No token → fuera
-  if (!token) {
+  if (!auth.getAccessToken()) {
     router.navigate(['/auth/login']);
     return false;
   }
 
-  // 1️⃣ Intentar validar token con /auth/me
-  return auth.me().pipe(
-    map(() => true),
-    catchError(() => {
-
-      const refresh = auth.getRefreshToken();
-      if (!refresh) {
-        router.navigate(['/auth/login']);
-        return of(false);
-      }
-
-      // 2️⃣ Intentar refresh token
-      return auth.refreshToken().pipe(
-        map((res) => {
-          if (res?.access_token) {
-            return true;
-          }
-          router.navigate(['/auth/login']);
-          return false;
-        }),
-        catchError(() => {
-          router.navigate(['/auth/login']);
-          return of(false);
-        })
-      );
-    })
-  );
+  return true;
 };
