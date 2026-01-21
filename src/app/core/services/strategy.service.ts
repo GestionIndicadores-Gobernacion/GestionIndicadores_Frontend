afbf59925e7f18cadb55d8c84d5c2e0a704645da
Milo <jcrodriguezc3@correo.usbcali.edu.co>
2026-01-21 09:59:18 -0500

    fix: production environment and secure api url

--- src/app/core/services/strategy.service.ts
+++ src/app/core/services/strategy.service.ts
@@ -1,7 +1,7 @@
 // core/services/strategies.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 import { 
   StrategyModel, 
   StrategyCreateRequest, 
--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 import { UserResponse, UserCreateRequest, UserUpdateRequest } from '../models/user.model';
 import { Observable } from 'rxjs';
 
