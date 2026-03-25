4bd9d35d10c939e1450461b1e102d2fa7c90b877
Milo <jcrodriguezc3@correo.usbcali.edu.co>
2026-03-25 17:52:18 -0500

    done 60%

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -4,41 +4,43 @@ import { environment } from '../../../environments/environment';
 import { UserResponse, UserCreateRequest, UserUpdateRequest } from '../models/user.model';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class UsersService {
   private api = `${environment.apiUrl}/users`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  /** Obtener todos los usuarios */
   getAll(): Observable<UserResponse[]> {
     return this.http.get<UserResponse[]>(this.api);
   }
 
-  /** Obtener usuario por ID */
   getById(id: number): Observable<UserResponse> {
     return this.http.get<UserResponse>(`${this.api}/${id}`);
   }
 
-  /** Obtener usuario actual (me) */
   getMe(): Observable<UserResponse> {
     return this.http.get<UserResponse>(`${this.api}/me`);
   }
 
-  /** Crear usuario */
   create(body: UserCreateRequest): Observable<UserResponse> {
     return this.http.post<UserResponse>(this.api, body);
   }
 
-  /** Actualizar usuario */
   update(id: number, body: UserUpdateRequest): Observable<UserResponse> {
     return this.http.put<UserResponse>(`${this.api}/${id}`, body);
   }
 
-  /** Eliminar usuario (soft delete) */
   delete(id: number): Observable<void> {
     return this.http.delete<void>(`${this.api}/${id}`);
+  }
+
+  /** Asignar un componente individual */
+  assignComponent(userId: number, componentId: number): Observable<any> {
+    return this.http.post(`${this.api}/${userId}/components/${componentId}`, {});
+  }
+
+  /** Quitar un componente individual */
+  removeComponent(userId: number, componentId: number): Observable<void> {
+    return this.http.delete<void>(`${this.api}/${userId}/components/${componentId}`);
   }
 }
